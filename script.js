081e7324bd5677ca3ea5f5df2d32f306e9aaa2f5
Kurisu003 <dani003@outlook.com>
2022-05-25 15:41:54 +0200

    Bread

---
+++ script.js
@@ -0,0 +1,29 @@
+let keyword = "bread";
+
+
+function changeKeywoard(str){
+    keyword = str;
+    updateBread();
+}
+
+function updateBread(){
+    $(".loading").show();
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+    {
+        tags: keyword,
+        tagmode: "any",
+        format: "json"
+    },
+    function(data) {
+        var rnd = Math.floor(Math.random() * data.items.length);
+
+        var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+
+        document.getElementById("breadImage").src = image_src;
+
+    });
+    $(".loading").hide();
+
+}
+
+$(document).ready(function(){updateBread();});
